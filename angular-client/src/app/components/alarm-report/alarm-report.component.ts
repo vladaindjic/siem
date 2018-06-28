a718e6247104940eff26eed7d626cc3b80c60ce1
Milan Desancic <milicamilan@gmail.com>
2018-06-28 06:24:22 +0200

    Odradjeni izvestaji

--- angular-client/src/app/components/alarm-report/alarm-report.component.ts
+++ angular-client/src/app/components/alarm-report/alarm-report.component.ts
@@ -2,55 +2,116 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import * as moment from 'moment';
 import { LogService } from '../../services/log.service';
 
+declare let jsPDF;
+
 @Component({
   selector: 'app-alarm-report',
   templateUrl: './alarm-report.component.html',
   styleUrls: ['./alarm-report.component.css']
 })
 export class AlarmReportComponent implements OnInit {
-  
+
   public selectedMoments = [];
   public selectedHost;
-  public hosts =['All'];
+  public hosts = ['All'];
   public report;
 
 
   public config = {
-    search:true //enables the search plugin to search in the list
-    };
+    search: true //enables the search plugin to search in the list
+  };
 
-    
-  constructor(private logService:LogService) {
-   }
- 
+
+  constructor(private logService: LogService) {
+  }
+
   ngOnInit() {
-    this.logService.getHosts().subscribe((data)=>{
+    this.logService.getHosts().subscribe((data) => {
       console.log(data);
       this.hosts = this.hosts.concat(data as Array<any>);
     })
   }
 
-  selectionChanged(eventValue){
+  selectionChanged(eventValue) {
     /*mozda nesto uradit*/
   }
 
 
-  getReport(){
+  getReport() {
     let data = {};
     data['startTime'] = moment(this.selectedMoments[0]).format();
     data['endTime'] = moment(this.selectedMoments[1]).format();
-    if(this.selectedHost.includes('All')){
+    if (this.selectedHost.includes('All')) {
       data['all'] = true;
-    }else{
+    } else {
       data['all'] = false;
     }
     data['hosts'] = this.selectedHost;
     //console.log(data);
-    this.logService.getAlarmAnalytics(data).subscribe((data)=>{
+    this.logService.getAlarmAnalytics(data).subscribe((data) => {
       data = JSON.parse(data as string);
       console.log(data);
       this.report = data;
     })
+  }
+
+
+  getPDF() {
+    var cols = ["appname", "facility", "hostname", "severity", "timestamp", "msg"];
+    var rows = [];
+    let i = 1;
+    var doc = new jsPDF('p', 'pt', 'a4');
+    let pageHeight = 842;
+
+
+    console.log(pageHeight);
+    for (let aggregation of this.report.aggregations) {
+
+      doc.setFontSize(12);
+      doc.setTextColor(0);
+      doc.setFontStyle('bold');
+
+      doc.text(10, i * 20, 'On host ' + aggregation._id.hostname + ' alarm fires ' + aggregation.count);
+      i++;
+      if (i * 20 >= pageHeight) {
+        doc.addPage();
+        i = 1;
+      }
+      for (let alarm_fires of aggregation.alarm_fires) {
+        doc.setFontSize(10);
+        doc.setTextColor(0);
+        doc.setFontStyle('normal');
+        doc.text(20, i * 20, '- alarm string: ' + alarm_fires.alarm_str);
+        i++;
+        if (i * 20 >= pageHeight) {
+          doc.addPage();
+          i = 1;
+        }
+        rows = [];
+        for (let log of alarm_fires.logs) {
+          var temp = [log['appname'], log['facility'], log['hostname'], log['severity'], moment(log['timestamp']).format(), log['msg']];
+          rows.push(temp);
+        }
+        doc.autoTable(cols, rows, {
+          startY: i*20,
+          styles: { overflow: 'linebreak' }, columnStyles: {
+            0: { columnWidth: 80 },
+            1: { columnWidth: 50 },
+            2: { columnWidth: 80 },
+            3: { columnWidth: 50 },
+            4: { columnWidth: 90 },
+            5: { columnWidth: 180 },
+          }
+        });
+        i = doc.autoTable.previous.finalY/20;
+        i++;
+
+      }
+
+
+
+    }
+    doc.save('pera.pdf');
   }
 
 }
--- angular-client/src/app/components/log-report/log-report.component.ts
+++ angular-client/src/app/components/log-report/log-report.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
 import * as moment from 'moment';
 import { LogService } from '../../services/log.service';
 import { ToastrService } from 'ngx-toastr';
-
+// import * as jsPDF from 'jspdf-autotable';
+// import * as html2canvas from 'html2canvas';
 declare let jsPDF;
+// declare var html2pdf: any;
 
 
 @Component({
@@ -12,80 +14,118 @@ declare let jsPDF;
   styleUrls: ['./log-report.component.css']
 })
 export class LogReportComponent implements OnInit {
-
+  @ViewChild('report') el: ElementRef;
   public selectedMoments = [];
   public selectedHost;
-  public hosts =['All'];
+  public hosts = ['All'];
   public report;
   public logs = [];
 
   public config = {
-    search:true //enables the search plugin to search in the list
-    };
+    search: true //enables the search plugin to search in the list
+  };
 
-  constructor(private logService:LogService,private toastr:ToastrService) { }
+  constructor(private logService: LogService, private toastr: ToastrService) { }
 
   ngOnInit() {
-    this.logService.getHosts().subscribe((data)=>{
+    this.logService.getHosts().subscribe((data) => {
       console.log(data);
       this.hosts = this.hosts.concat(data as Array<any>);
     })
   }
 
-  getReport(){
+  getReport() {
     let data = {};
     data['startTime'] = moment(this.selectedMoments[0]).format();
     data['endTime'] = moment(this.selectedMoments[1]).format();
     console.log(this.selectedHost);
-   
-    if(this.selectedHost.includes('All')){
+
+    if (this.selectedHost.includes('All')) {
       data['all'] = true;
-    }else{
+    } else {
       data['all'] = false;
     }
     data['hosts'] = this.selectedHost;
-    //console.log(data);
-    this.logService.getLogAnalytics(data).subscribe((data)=>{
+    console.log(data);
+    this.logService.getLogAnalytics(data).subscribe((data) => {
       data = JSON.parse(data as string);
       console.log(data);
-      if(data['count']===0){
+      if (data['count'] === 0) {
         this.toastr.error('0 results for this query')
-      }else{
+      } else {
         this.report = data;
-        for(let aggregation of data['aggregations']){
-            this.logs = this.logs.concat(aggregation.logs);
+        this.logs = [];
+        for (let aggregation of data['aggregations']) {
+          this.logs = this.logs.concat(aggregation.logs);
         }
         console.log(this.logs);
       }
     },
-    (error) => {
-      this.toastr.error('Incorrect use of query');
-    })
+      (error) => {
+        this.toastr.error('Incorrect use of query');
+      })
   }
 
-  selectionChanged(eventValue){
-    /*mozda nesto uradit*/
+  selectionChanged(eventValue) {
   }
 
-  savePDF(){
-    var doc = new jsPDF('p', 'pt');
-    var col = ["appname", "facility","hostname","severity","timestamp","msg"];
+  savePDF() {
+
+    // let pdf = new jsPDF();
+
+    // pdf.addHTML(this.el.nativeElement, () => {
+    //   pdf.save("test1.pdf");
+    // });
+
+    // let doc = new jsPDF();
+
+    // // Add a title to your PDF
+    // doc.setFontSize(30);
+    // doc.text(12, 10, "Your Title");
+
+    // Create your table here (The dynamic table needs to be converted to canvas).
+    // let element = <HTMLScriptElement>document.getElementsByClassName("pvtTable")[0];
+    // html2canvas(element)
+    //   .then((canvas: any) => {
+    //     doc.addImage(canvas.toDataURL("image/jpeg"), "JPEG", 0, 50, doc.internal.pageSize.width, element.offsetHeight / 5);
+    //     doc.save(`Report-${Date.now()}.pdf`);
+    //   })
+
+    //   html2canvas(("#canvas"), {
+    //     onrendered: function(canvas) {         
+    //         var imgData = canvas.toDataURL(
+    //             'image/png');              
+    //         var doc = new jsPDF('p', 'mm');
+    //         doc.addImage(imgData, 'PNG', 10, 10);
+    //         doc.save('sample-file.pdf');
+    //     }
+    // });
+    var doc = new jsPDF('p', 'pt','a4');
+    var cols = ["appname", "facility", "hostname", "severity", "timestamp", "msg"];
     var rows = [];
 
-    for(var log of this.logs){
-        var temp = [log['appname'],log['facility'],log['hostname'],log['severity'],moment(log['timestamp']).format(),log['msg']];
-        rows.push(temp);
+    for (var log of this.logs) {
+      var temp = [log['appname'], log['facility'], log['hostname'], log['severity'], moment(log['timestamp']).format(), log['msg']];
+      rows.push(temp);
     }
-    console.log(col)
-    console.log(rows)
-    doc.autoTable(col, rows,{ 
-      margin: {horizontal: 7},
-      bodyStyles: {valign: 'top'},
-      styles: {overflow: 'linebreak', columnWidth: 'wrap'},
-      columnStyles: {text: {columnWidth: 'auto'}}
-  });
-  let name = "repot_"+  moment(new Date).format();
-  doc.save(name+'.pdf');
-  return doc;
-}
+
+    // doc.autoTable(cols, rows, {
+    //     margin: {horizontal: 7},
+    //     bodyStyles: {valign: 'top'},
+    //     styles: {overflow: 'linebreak', columnWidth: 'wrap'},
+    //     columnStyles: {text: {columnWidth: 'auto'}}
+    // });
+
+    doc.autoTable(cols, rows, {styles: {overflow: 'linebreak'},columnStyles: {
+      0: {columnWidth: 80},
+      1: {columnWidth: 50},
+      2: {columnWidth: 80},
+      3: {columnWidth: 50},
+      4: {columnWidth: 90},
+      5: {columnWidth: 180},
+  }});
+    let name = "repot_" + moment(new Date).format();
+    doc.save(name + '.pdf');
+    // return doc;
+  }
 }
